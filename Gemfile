1d80b7e852cc2a4f6b3bfa23e3f00def02ef332e
nozomi-iida <iida19990106@gmail.com>
2021-02-04 20:26:09 +0900

    impl: jwt on the way

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'puma', '~> 5.0'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'graphql', '~> 1.11'
+gem 'jwt', '~> 2.2'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
---
+++ lib/j_w_t/helper.rb
@@ -0,0 +1,13 @@
+module JWT
+  module Helper
+    module_function
+
+    def encode(payload, alg = 'HS256', typ = 'JWT', **opts)
+      JWT.encode(payload, Rails.application.secrets.secret_key_base, alg, typ: typ, **opts)
+    end
+
+    def decode(token, verify: true, **opts)
+      JWT.decode(token, Rails.application.secrets.secret_key_base, verify, **opts)
+    end
+  end
+end
