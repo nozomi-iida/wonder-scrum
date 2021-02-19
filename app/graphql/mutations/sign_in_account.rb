bb30487004b4c23db1ee160d9374219ffbdf41b5
nozomi-iida <iida19990106@gmail.com>
2021-02-19 09:50:00 +0900

    feat: sign in account

---
+++ app/graphql/mutations/sign_in_account.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Mutations
+  # SignInAccount
+  class SignInAccount < BaseMutation
+    field :account, Types::AccountType, null: false
+
+    argument :email, String, required: true
+    argument :password, String, required: true
+
+    def resolve(args)
+      account = Account.find_by(
+        email: args[:email]
+      ).try(:authenticate, args[:password])
+
+      return unless account
+
+      {
+        account: account,
+        token: account.jwt
+      }
+    end
+  end
+end
