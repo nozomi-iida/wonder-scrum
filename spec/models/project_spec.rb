67d9329803902ca578ec6dc6f95ce57a23e006ec
nozomi-iida <iida19990106@gmail.com>
2021-03-07 09:09:54 +0900

    fix: remove model test and setted rubocop

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe Project, skip: true, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Project, type: :model do
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe Task, skip: true, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Task, type: :model do
 end
